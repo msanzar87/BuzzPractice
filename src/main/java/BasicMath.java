74132daff0c75f2dec103c4fe2c7c04dae1941b7
unknown <m.buzzard87@gmail.com>
2021-09-10 21:47:21 -0400

    update pom and add BasicMath

---
+++ src/main/java/BasicMath.java
@@ -0,0 +1,98 @@
+public class BasicMath {
+
+    /*
+    Create 3 tests for each method
+
+    - Right click on BasicMath, scroll down and select 'Generate'
+    - Click on 'Test...', select all the methods below and click 'OK'
+    - Instantiate BasicMath within the BasicMathTest class to call on methods within BasicMath
+
+
+    Example:
+
+    import org.junit.Test;  <------ Make sure this is imported already
+    import static org.junit.Assert.*;  <------ Import this
+
+    public class BasicMathTest {
+
+    BasicMath bm = new BasicMath();  <------ Instantiated
+
+       @Test
+       public void add1() {
+
+       int actual = bm.add(1,2);
+
+       assertEquals(3,actual);
+
+       }
+
+   }
+
+
+     */
+
+
+    /**
+     * @param num1 - first number
+     * @param num2 - second number
+     * @return an int of the sum of num1 and num2
+     */
+    public int add(int num1, int num2) {
+
+
+        return 0;
+    }
+
+
+
+    /**
+     * @param num1 - first number
+     * @param num2 - second number
+     * @return an int of the difference between num1 and num2
+     */
+    public int subtract(int num1, int num2) {
+
+
+        return 0;
+    }
+
+
+
+    /**
+     * @param num1 - first number
+     * @param num2 - second number
+     * @return an int from num1 multiplied by num2
+     */
+    public int multiply(int num1, int num2) {
+
+
+        return 0;
+    }
+
+
+    /**
+     * @param num1 - first number
+     * @param num2 - second number
+     * @return an int from num1 divided by num2
+     * (only enter numbers that will return whole/negative numbers only in the test methods)
+     */
+    public int divide(int num1, int num2) {
+
+
+        return 0;
+    }
+
+
+
+    /**
+     * @param num1 - first number
+     * @param num2 - second number
+     * @return an int with the remainder of num1 divided by num2
+     */
+    public int modulo(int num1, int num2) {
+
+
+        return 0;
+    }
+
+}
