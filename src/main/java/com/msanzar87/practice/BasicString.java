13ccb8cb4b6a575c8281aaa60b658b6da228a4cd
munir sanzar <msanzar87@gmail.com>
2021-09-20 15:53:36 -0400

    basic string 1-3

--- src/main/java/com/msanzar87/practice/BasicString.java
+++ src/main/java/com/msanzar87/practice/BasicString.java
@@ -1,9 +1,24 @@
 package com.msanzar87.practice;
 
 public class BasicString {
+    public char firstChar(String str) {
+
+        return str.charAt(0);
+    }
+
+    public char secondToLastChar(String str) {
+        return str.charAt(str.length()-2);
+    }
+
+    public boolean containsLetter(String str, String c) {
+        return str.contains(c);
+    }
     // Create tests before writing the methods below in this class
     // 1. Write a method that returns the first character of a String
+    //start with test class and then generate method into regular class
     // 2. Write a method that returns the second to last character of a String
+
+
     // 3. Write a method that returns a boolean value to check if the string contains a character
     // 4. Write a method that returns a boolean value to check if the string is a palindrome
 
---
+++ src/test/java/com/msanzar87/practice/BasicStringTest.java
@@ -0,0 +1,35 @@
+package com.msanzar87.practice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BasicStringTest {
+    BasicString bs = new BasicString();
+    //instantiate class into test class (class instantiated becomes object)
+
+    @Test
+    public void test1stChar(){
+        char expected = 't';
+
+        char actual = bs.firstChar("test");
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void test2ndToLastChar(){
+        char expected = 's';
+
+        char actual = bs.secondToLastChar("test");
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void testContainsChar(){
+        String contains = "e";
+
+        boolean actual = bs.containsLetter("test", contains);
+
+        assertTrue(actual);
+    }
+}
