8d3a55d80b839d898a852d31db99b9c4264d7f4d
unknown <m.buzzard87@gmail.com>
2021-09-23 20:30:00 -0400

    applestore jawn

---
+++ src/main/java/MainAppleStoreApplication.java
@@ -0,0 +1,19 @@
+public class MainAppleStoreApplication {
+
+
+    public static void main(String[] args) {
+        //Create an apple store and fill all the parameters with values
+
+        //Create 3 customers and fill all the parameters with values
+
+
+
+
+        /*
+        Have each customer come in and purchase an apple. Print the customer name, the number of apples they bought.
+
+        Separately print the updated values of the store after each customer makes a purchase
+         */
+
+    }
+}
---
+++ src/main/java/com/msanzar87/practice/store/Customer.java
@@ -0,0 +1,6 @@
+package com.msanzar87.practice.store;
+
+public class Customer {
+
+    //Create a customer entity with the fields: name and number of apples purchased
+}
