bcfd0dbf93580c45a5ed2e4d4eb6fdea0d743a52
unknown <m.buzzard87@gmail.com>
2021-09-25 23:21:16 -0400

    duplicates

---
+++ src/main/java/com/msanzar87/practice/Duplicates.java
@@ -0,0 +1,12 @@
+package com.msanzar87.practice;
+
+public class Duplicates {
+
+    //WRITE TESTS FIRST!!!  ADD, COMMIT, PUSH OFTEN!!!
+
+    //Create a method that returns true or false if an int array contains a boolean
+
+    //Create a method that returns an int array leaving out any duplicate numbers
+
+    //Create a method that returns an int with the number of duplicate values within an array
+}
