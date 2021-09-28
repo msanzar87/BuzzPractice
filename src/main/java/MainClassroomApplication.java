d7ca199752975b89f755e3cc4601e7e00fa20621
munir sanzar <msanzar87@gmail.com>
2021-09-27 20:45:13 -0400

    Completed Classroom

--- src/main/java/MainClassroomApplication.java
+++ src/main/java/MainClassroomApplication.java
@@ -10,16 +10,23 @@ public class MainClassroomApplication {
 
     public static void main(String[] args) {
         // Create an ArrayList and add 5 students
-        List<Student> students = new ArrayList<Student>();
-        Student s1 = new Student("Buzz", 'M', 34);
-        Student s2 = new Student("Munir",'M',34);
-        Student s3 = new Student("Christian",'M', 42);
-        Student s4 = new Student("Laura",'F',31);
-        Student s5 = new Student("Melissa",'F',28);
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("Buzz", 'M', 34));
+        students.add(new Student("Munir",'M',34));
+        students.add(new Student("Christian",'M', 42));
+        students.add(new Student("Laura",'F',31));
+        students.add(new Student("Melissa",'F',28));
+        students.add(new Student("Pete",'N',27));
+
         // Create a classroom for the list of students to be enrolled in
-        Classroom cs = new Classroom();
+        Classroom mathClass = new Classroom(students);
 
         // Iterate through the classroom and print each classmates name and gender
+        for (int i = 0; i < ; i++) {
+
+        }
+
 
 
 
--- src/main/java/com/msanzar87/practice/classroom/Classroom.java
+++ src/main/java/com/msanzar87/practice/classroom/Classroom.java
@@ -1,10 +1,25 @@
 package com.msanzar87.practice.classroom;
 
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Classroom {
 
+    private List<Student> students;
 
+    public Classroom(){}
 
-    //Create a classroom entity that contains a "LIST" of all the students in a class
+    public Classroom(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+//Create a classroom entity that contains a "LIST" of all the students in a class
 }
